22e33dc0b58bea62f38b28620a0b80bc9c37b3a6
Mohamed Abdoul Karim Diop <mohamedabdoulkarim.diop2@orange-sonatel.com>
2023-10-09 10:44:54 +0000

    fix: Add firebase config for PPROD environment

--- src/environments/environment.preprod.ts
+++ src/environments/environment.preprod.ts
@@ -3,12 +3,12 @@ export const environment = {
     AIRTABLE_KEY: 'pateDROHP7ZSHrACq.4f09efd0cabff4a184baabcb4a759a5782fef97c25bc95d6c7aa12fc66e74821',
     AIRTABLE_BASE_ID: 'appDfgbuOU3PUPSOK',
     FIREBASE_CONFIG : {
-        apiKey: "AIzaSyDXV5FtdHEDEYe-r0eZhiVgQye4w2lI4RI",
-        authDomain: "job-dating-dch-pasteur.firebaseapp.com",
-        projectId: "job-dating-dch-pasteur",
-        storageBucket: "job-dating-dch-pasteur.appspot.com",
-        messagingSenderId: "50085832371",
-        appId: "1:50085832371:web:6758fc206b85a68d9d32c6",
-        measurementId: "G-MWQNR5YDX0"
+      apiKey: "AIzaSyD13fi4ThY9PXp09nyTj0jg79y35GtHl1k",
+      authDomain: "job-pasteur-dating-app.firebaseapp.com",
+      projectId: "job-pasteur-dating-app",
+      storageBucket: "job-pasteur-dating-app.appspot.com",
+      messagingSenderId: "500469284979",
+      appId: "1:500469284979:web:f4db312ecef8d349f22a06",
+      measurementId: "G-RRLFYPLJ4X"
       }
 };
